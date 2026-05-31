4ba1491f645fedc1a71c3cf48f9ff664b726214a
mkarounos4 <mkarounos4@gmail.com>
2026-05-31 18:17:55 -0400

    [VM] Cleanup virtual memory

--- kernel/memory/pages/paging.c
+++
@@ -1,8 +0,0 @@
-#include "paging.h"
-
-void page_table_init(void *ustack, void *uprogram, void *memory, size_t memory_size) {
-    (void)ustack;
-    (void)uprogram;
-    (void)memory;
-    (void)memory_size;
-}
